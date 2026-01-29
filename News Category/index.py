6d04c6ac10a2f8d9f383ce3cc4aeeb63ad9cc37c
Rohit <Roshankr2223@gmail.com>
2026-01-29 20:38:31 +0545

    first commit

---
+++ News Category/index.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import joblib
+
+model=joblib.load('model.joblib')
+
+st.title('News Category Prediction')
+st.markdown('Enter news below')
+input_text=st.text_area(
+    label='Enter news text that you want to predict',
+    max_chars=1000, height=300)
+
+if st.button("Predict Category"):
+    prediction=model.predict([input_text])[0]
+    st.success(f'Predicted Category is {prediction}')
